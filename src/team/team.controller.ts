af1e32a60aa3ae8ab64512dbbb3167ddaf11274a
Abdulrehman-Jafer <abdulrehmanjaferworks01233@gmail.com>
2024-08-27 12:09:27 +0500

    controller

--- src/team/team.controller.ts
+++ src/team/team.controller.ts
@@ -16,6 +16,10 @@ import { User } from 'src/common/decorator/user.decorator';
 import { Jwt_Auth_Gurad } from 'src/auth/jwt_auth.guard';
 import { Trasnform_BigInt_To_String } from 'src/common/interceptors/transform_big_int_to_string.interceptor';
 import { TeamService } from './team.service';
+import { Edit_Team_Request_DTO } from './dto/edit_team_request.dto';
+import { Parse_BigInt_Pipe } from 'src/common/custom_pipes/transform_to_big_int.pipe';
+import { Edit_Team_Name_Request_DTO } from './dto/edit_team_name_request.dto';
+import { Edit_Team_Remarks_Request_DTO } from './dto/edit_team_remarks_request.dto';
 
 @UseGuards(Jwt_Auth_Gurad)
 @UseInterceptors(Trasnform_BigInt_To_String)
@@ -27,7 +31,11 @@ export class TeamController {
   ) {}
 
   @Get('/') // AddALl
-  async get_all_teams() {}
+  async get_all_teams() {
+    return this.util_service.tryCatchWrapper(() =>
+      this.team_service.get_all(),
+    )();
+  }
 
   @Post('/') // /New
   async new_team(
@@ -40,32 +48,80 @@ export class TeamController {
   }
 
   @Patch('/') // Edit
-  async update_team() {}
+  async update_team(@Body() request_dto: Edit_Team_Request_DTO) {
+    return this.util_service.tryCatchWrapper(() =>
+      this.team_service.update(request_dto),
+    )();
+  }
 
   @Delete('/') // Del
-  async delete_team() {}
+  async delete_team(
+    @Query('id', Parse_BigInt_Pipe) id: bigint,
+    @User() user: user_with_role_and_urls_with_id_as_bigInt,
+  ) {
+    return this.util_service.tryCatchWrapper(() =>
+      this.team_service.delete(id, user),
+    )();
+  }
 
   @Patch('/name') //EditName
-  async update_team_name() {}
+  async update_team_name(@Body() request_dto: Edit_Team_Name_Request_DTO) {
+    return this.util_service.tryCatchWrapper(() =>
+      this.team_service.update_name(request_dto),
+    )();
+  }
 
   @Patch('/remark') // EditRea
-  async update_team_remarks() {}
+  async update_team_remarks(
+    @Body() request_dto: Edit_Team_Remarks_Request_DTO,
+  ) {
+    return this.util_service.tryCatchWrapper(() =>
+      this.team_service.update_remarks(request_dto),
+    )();
+  }
 
   @Get('/id') //GetById
-  async get_team_by_id() {}
+  async get_team_by_id(
+    @Query('id', Parse_BigInt_Pipe) id: bigint,
+    @User() user: user_with_role_and_urls_with_id_as_bigInt,
+  ) {
+    return this.util_service.tryCatchWrapper(() =>
+      this.team_service.get_by_id(id, user),
+    )();
+  }
 
-  @Get('/name') //GetBName
-  async get_team_by_name() {}
+  @Get('/name') //ByName
+  async get_team_by_name() {
+    return this.util_service.tryCatchWrapper(() =>
+      this.team_service.update(request_dto),
+    )();
+  }
 
   @Get('/getByMy') // getByMy
-  async get_by_my() {}
+  async get_by_my() {
+    return this.util_service.tryCatchWrapper(() =>
+      this.team_service.update(request_dto),
+    )();
+  }
 
   @Get('/team_mates') // GetTeamMates
-  async get_team_mates() {}
+  async get_team_mates() {
+    return this.util_service.tryCatchWrapper(() =>
+      this.team_service.update(request_dto),
+    )();
+  }
 
   @Get('/transfer_mates') // GetTeTrasnfermates
-  async get_te_transfer_mates() {}
+  async get_te_transfer_mates() {
+    return this.util_service.tryCatchWrapper(() =>
+      this.team_service.update(request_dto),
+    )();
+  }
 
   @Get('/code') // ReloadCode
-  async get_code() {}
+  async get_code() {
+    return this.util_service.tryCatchWrapper(() =>
+      this.team_service.update(request_dto),
+    )();
+  }
 }
